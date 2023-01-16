ebc4c06affd726199dd4ba2ff25ca8066588d9cb
Athithyan Rengaswamy <atr@Athithyans-MacBook-Pro.local>
2023-01-16 18:57:20 +0530

    dataset cleared to reduce size from 5.1 to 2.9mb and binary search added

---
+++ gen-data/index.js
@@ -0,0 +1,42 @@
+const { writeFileSync } = require("fs");
+const data = require("../data/data.json");
+
+
+function merge(arr1, arr2) {
+  const result = [];
+  let i = 0,
+    j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    const res = arr1[i][1].localeCompare(arr2[j][1])
+    if (res <= 0) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+// writeFileSync('./data/data-2.json', JSON.stringify(mergeSort(data)), { encoding: 'utf-8' })
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 #!/usr/bin/env -S node --experimental-vm-modules
 "use strict";
 
-const args = process.argv.slice(2)
-if (args.length < 1 || args.length > 1) {
-    console.error('Invalid params provided')
-    process.exit(1)
-}
-const [city] = args;
+const data = require("../data/data.json");
 
-const data = require('../data/data.json')
-console.log(data.find((obj) => obj.city_ls.includes(city)) || 'No matches found')
+function binarySearchCity(city_name) {
+  let l = 0,
+    r = data.length - 1;
+
+  while (l <= r) {
+    const mid = l + parseInt((r - l) / 2);
+    if (data[mid][1].startsWith(city_name)) return data[mid];
+    if (city_name < data[mid][1]) r = mid - 1;
+    else l = mid + 1;
+  }
+
+  return;
+}
+
+function findByCity(query_city) {
+  console.time("Searching");
+  // TC -  1.692ms, 1.56ms, 1.556ms, 1.7ms, 1.568ms, 1.56ms
+  // const found = data.find((element) => element[1].startsWith(query_city));
+  // TC - 0.084ms, 0.078ms, 0.08ms, 0.081ms, 0.088ms, 0.076ms
+  const found = binarySearchCity(query_city);
+  console.timeEnd("Searching");
+  if (!found) return console.log("No matches found");
+
+  const [city, city_ls, lat, lng, country, population] = found;
+  console.log({ city, city_ls, lat, lng, country, population });
+}
+
+const args = process.argv.slice(2);
+if (args.length < 1 || args.length > 1) {
+  console.error("Invalid params provided");
+  process.exit(1);
+}
+const [query_city] = args;
+findByCity(query_city);
